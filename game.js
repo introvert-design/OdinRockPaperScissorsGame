8133eea33422240ad29e5efc99e71f01da64f692
GR <geo.rosy+git@protonmail.ch>
2022-02-12 03:30:03 +0530

    Create UI

--- game.js
+++ game.js
@@ -1,50 +1,60 @@
 const TOOLS = ["Rock", "Paper", "Scissors"]
+let machineScore = 0;
+let humanScore = 0;
 
-function computerPlay() {
-    return TOOLS[Math.floor(Math.random()*3)];
+const buttons = () => document.querySelectorAll('.user-choice');
+const restart = () => document.querySelector('.restart');
+
+const computerPlay = () => TOOLS[Math.floor(Math.random()*3)];
+
+function disableButton(button) {
+    button.classList.add('disabled');
+    button.disabled = true;
 }
 
-function playRound(playerSelection, computerSelection, win) {
+function displayResult(elt, content) {
+    const selectElement = document.querySelector(elt)
+    selectElement.textContent = content;
+}
+
+function resetGame() {
+    location.reload();
+}
+
+function playRound(event) {
+    computerSelection = computerPlay();
+    playerSelection = event.target.value;
     if (playerSelection == computerSelection) {
-        return [`You Tie! ${playerSelection} cancels ${computerSelection}`, win];
+        content = `You Tie! ${playerSelection} cancels ${computerSelection}`;
+        displayResult('.play-round-result', content)
     }
     else if ((playerSelection == TOOLS[0] && computerSelection == TOOLS[1]) 
         || (playerSelection == TOOLS[1] && computerSelection == TOOLS[2])
         || (playerSelection == TOOLS[2] && computerSelection == TOOLS[0])) {
-            return [`You Lose! ${computerSelection} beats ${playerSelection}`, --win];
-    }
-    else if ((playerSelection == TOOLS[0] && computerSelection == TOOLS[2]) 
-        || (playerSelection == TOOLS[1] && computerSelection == TOOLS[0])
-        || (playerSelection == TOOLS[2] && computerSelection == TOOLS[1])) {
-            return [`You Win! ${playerSelection} beats ${computerSelection}`, ++win];
+            content = `You Lose! ${computerSelection} beats ${playerSelection}`;
+            displayResult('.play-round-result', content);
+            machineScore++;
+            displayResult('.machine-score', machineScore);
+            if (machineScore === 5) {
+                content = 'You lost! Game Over...';
+                displayResult('.final-result', content);
+            }
     }
     else {
-        return ["", win];
+        content = `You Win! ${playerSelection} beats ${computerSelection}`;
+        displayResult('.play-round-result', content);
+        humanScore++;
+        displayResult('.human-score', humanScore);
+        if (humanScore === 5) {
+            content = 'You beat the crap out of machines!';
+            displayResult('.final-result', content);
+        }
+    }
+    if (machineScore >= 5 || humanScore >= 5) {
+        buttons().forEach(button => disableButton(button));
+        restart().style.display = 'inline-block';
     }
 }
 
-let playerSelection = () => 
-    prompt("What do you choose? (Rock, Paper or Scissors)")
-        .trim()
-        .toLowerCase()
-        .replace(/^\w/, (c) => c.toUpperCase());
-
-function game() {
-    let win = 0;
-    let message = null;
-    for (let i = 0; i < 5; i++) {
-        result = playRound(playerSelection(), computerPlay(), win);
-        message = result[0];
-        win = result[1]
-        if (message == "") {
-            console.log("Invalid input! Only Rock, Paper or Scissors allowed");
-            i--
-        }
-        else {
-            console.log(message);
-            console.log(win);
-        }
-    }
-}
-
-game();
+buttons().forEach(button => button.addEventListener('click', playRound));
+restart().addEventListener('click', resetGame);
